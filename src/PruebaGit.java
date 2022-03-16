f05518b331602cb5cefa28dd5ca1ce88996fd366
Carlos <Carlos@DESKTOP-N96QJ47>
2022-03-16 13:34:34 +0100

    Primer commit de la integración Eclipse-GitHub

---
+++ src/PruebaGit.java
@@ -0,0 +1,12 @@
+
+public class PruebaGit {
+	public static void main(String[] args) {
+		System.out.println("Hola mundo");
+		System.out.println("Adiós mundo");
+		System.out.println("Hola de nuevo");
+		String nombre = "Carlos Duato";
+		System.out.println("Adios de nuevo");
+		System.out.println("PD: mi nombre es "+nombre);
+	}
+
+}
